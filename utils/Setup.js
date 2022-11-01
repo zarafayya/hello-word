ef96f15d3d99b8c9bd6e8b36490fb634c43b4d80
Nathanael Roviery <roviery@gmail.com>
2022-11-01 15:10:30 +0700

    fix: perspective camera aspect

--- utils/Setup.js
+++ utils/Setup.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 
 export default function Setup() {
   const scene = new THREE.Scene();
-  const perspectiveCamera = new THREE.PerspectiveCamera(75, window.innerHeight / window.innerHeight, 0.1, 1000);
+  const perspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
   });
