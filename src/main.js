79dc84bdcee394e6a2c388598674748a69c33b1a
SRYian <92420947+SRYian@users.noreply.github.com>
2022-11-29 22:21:03 +0700

    feat: add gltf loader for models

--- src/main.js
+++ src/main.js
@@ -1,102 +1,128 @@
-import './style.css';
-import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-import Setup from './utils/Setup';
-import Lighting from './utils/Lighting';
-import Plane from './utils/Plane';
-import Model from './utils/Model';
-import gsap from 'gsap';
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import Setup from "./utils/Setup";
+import Lighting from "./utils/Lighting";
+import Plane from "./utils/Plane";
+import Model from "./utils/Model";
+import gsap from "gsap";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 // Setup
-const {scene, perspectiveCamera, renderer} = Setup();
+const { scene, perspectiveCamera, renderer } = Setup();
 const tl = gsap.timeline();
-window.addEventListener('mousedown', () => {
+window.addEventListener("mousedown", () => {
   tl.to(perspectiveCamera.position, {
     z: 100,
     duration: 2,
-  })
-  .to(perspectiveCamera.position, {
+  }).to(perspectiveCamera.position, {
     z: -100,
     duration: 2,
-  })
-})
-
+  });
+});
 
 // Plane
 Plane(scene);
 
+// Textures
+
 // Model
-Model(scene, './assets/model/A.gltf', 5, 10, 0);
-Model(scene, './assets/model/B.gltf', 5, 10, 200);
-Model(scene, './assets/model/C.gltf', -250, 10, 200);
+Model(scene, "./assets/model/A.gltf", 5, 10, 0);
+Model(scene, "./assets/model/B.gltf", 5, 10, 200);
+Model(scene, "./assets/model/C.gltf", -250, 10, 200);
 perspectiveCamera.lookAt(5, 10, 0);
 
+// loader for GLTF D model
+const gltfLoader = new GLTFLoader();
+
+gltfLoader.load("./assets/model/D.gltf", (gltf) => {
+  gltf.scene.children.forEach((element) => {
+    const obj = element.getObjectByName(element.name);
+    obj.traverse(function (node) {
+      if (node.isMesh) {
+        const material = new THREE.MeshPhongMaterial({
+          color: 0xff0000, // red (can also use a CSS color string here)
+          flatShading: true,
+          shininess: 100,
+        });
+        let color = new THREE.Color(0xaa5511);
+        material.color = color;
+        node.material = material;
+      }
+    });
+  });
+
+  gltf.scene.scale.set(20, 20, 20);
+  gltf.scene.position.set(5, 10, 300);
+  gltf.scene.rotateY(3.14159);
+  scene.add(gltf.scene);
+});
+// scene.add(obj, material);
+
 // Lighting
-const {pointLight} = Lighting(0, 100, -25);
+const { pointLight } = Lighting(0, 100, -25);
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
 // Keys
-window.addEventListener('keydown', (e) => {
+window.addEventListener("keydown", (e) => {
   var x = 0;
   var y = 0;
   var z = 0;
   switch (e.key) {
-    case 'ArrowUp':
+    case "ArrowUp":
       y = 10;
       break;
-    case 'ArrowDown':
+    case "ArrowDown":
       y = -10;
       break;
-    case 'ArrowLeft':
+    case "ArrowLeft":
       x = 10;
       break;
-    case 'ArrowRight':
+    case "ArrowRight":
       x = -10;
       break;
-    case 'z':
+    case "z":
       z = 10;
       break;
-    case 'x':
+    case "x":
       z = -10;
       break;
   }
   perspectiveCamera.position.x += x;
   perspectiveCamera.position.y += y;
   perspectiveCamera.position.z += z;
-})
+});
 
 // rotate
-window.addEventListener('keydown', (e) => {
-switch (e.key) {
-  case 'd':
-    perspectiveCamera.rotation.y += 0.1;
-    break;
-  case 'a':
-    perspectiveCamera.rotation.y -= 0.1;
-    break;
-  case 's':
-    perspectiveCamera.rotation.x += 0.1;
-    break;
-  case 'w':
-    perspectiveCamera.rotation.x -= 0.1;
-    break;
-}
-})
-
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "d":
+      perspectiveCamera.rotation.y += 0.1;
+      break;
+    case "a":
+      perspectiveCamera.rotation.y -= 0.1;
+      break;
+    case "s":
+      perspectiveCamera.rotation.x += 0.1;
+      break;
+    case "w":
+      perspectiveCamera.rotation.x -= 0.1;
+      break;
+  }
+});
 
 // Map Texture
 const loader = new THREE.CubeTextureLoader();
 const skyBox = loader.load([
-  './assets/img/px.png',
-  './assets/img/nx.png',
-  './assets/img/py.png',
-  './assets/img/ny.png',
-  './assets/img/pz.png',
-  './assets/img/nz.png',
+  "./assets/img/px.png",
+  "./assets/img/nx.png",
+  "./assets/img/py.png",
+  "./assets/img/ny.png",
+  "./assets/img/pz.png",
+  "./assets/img/nz.png",
 ]);
 scene.background = skyBox;
-
 
 function animate() {
   requestAnimationFrame(animate);
