5b5c1d3aa96b43dff1f35821fd292c1ba8a9059b
Trelel129 <ferrynur77@gmail.com>
2022-12-10 15:42:22 +0700

    feat: DEF card coordinates

--- src/utils/Card.js
+++ src/utils/Card.js
@@ -19,17 +19,17 @@ const Card = {
     D: {
         name: "cardD",
         model: "./assets/model/cards/cardD.gltf",
-        position: new THREE.Vector3(135, 20, 80)
+        position: new THREE.Vector3(-80, 4, 135)
     },
     E: {
         name: "cardE",
         model: "./assets/model/cards/cardE.gltf",
-        position: new THREE.Vector3(-135, 20, 160)
+        position: new THREE.Vector3(100, 4, 180)
     },
     F: {
         name: "cardF",
         model: "./assets/model/cards/cardF.gltf",
-        position: new THREE.Vector3(0, 20, 160)
+        position: new THREE.Vector3(-30, 4, 225)
     },
 }
 
