dfecce58ab82252b975fe071e12dc6d8ab17cda7
zahrafayya <zahra.fayyadiyati@gmail.com>
2022-12-11 11:34:26 +0700

    feat: add card object

--- src/utils/Card.js
+++ src/utils/Card.js
@@ -18,17 +18,117 @@ const Card = {
     },
     D: {
         name: "cardD",
-        model: "./assets/model/cards/cardD.gltf",
+        model: "./assets/model/cards/cardD.glb",
         position: new THREE.Vector3(-80, 4, 135)
     },
     E: {
         name: "cardE",
-        model: "./assets/model/cards/cardE.gltf",
+        model: "./assets/model/cards/cardE.glb",
         position: new THREE.Vector3(100, 4, 180)
     },
     F: {
         name: "cardF",
-        model: "./assets/model/cards/cardF.gltf",
+        model: "./assets/model/cards/cardF.glb",
+        position: new THREE.Vector3(-30, 4, 225)
+    },
+    G: {
+        name: "cardG",
+        model: "./assets/model/cards/cardG.glb",
+        position: new THREE.Vector3(-30, 4, 225)
+    },
+    H: {
+        name: "cardH",
+        model: "./assets/model/cards/cardH.glb",
+        position: new THREE.Vector3(-30, 4, 225)
+    },
+    I: {
+        name: "cardI",
+        model: "./assets/model/cards/cardI.glb",
+        position: new THREE.Vector3(-30, 4, 225)
+    },
+    J: {
+        name: "cardJ",
+        model: "./assets/model/cards/cardJ.glb",
+        position: new THREE.Vector3(-30, 4, 225)
+    },
+    K: {
+        name: "cardK",
+        model: "./assets/model/cards/cardK.glb",
+        position: new THREE.Vector3(-30, 4, 225)
+    },
+    L: {
+        name: "cardL",
+        model: "./assets/model/cards/cardL.glb",
+        position: new THREE.Vector3(-30, 4, 225)
+    },
+    M: {
+        name: "cardM",
+        model: "./assets/model/cards/cardM.glb",
+        position: new THREE.Vector3(-30, 4, 225)
+    },
+    N: {
+        name: "cardN",
+        model: "./assets/model/cards/cardN.glb",
+        position: new THREE.Vector3(-30, 4, 225)
+    },
+    O: {
+        name: "cardO",
+        model: "./assets/model/cards/cardO.glb",
+        position: new THREE.Vector3(-30, 4, 225)
+    },
+    P: {
+        name: "cardP",
+        model: "./assets/model/cards/cardP.glb",
+        position: new THREE.Vector3(-30, 4, 225)
+    },
+    Q: {
+        name: "cardQ",
+        model: "./assets/model/cards/cardQ.glb",
+        position: new THREE.Vector3(-30, 4, 225)
+    },
+    R: {
+        name: "cardR",
+        model: "./assets/model/cards/cardR.glb",
+        position: new THREE.Vector3(-30, 4, 225)
+    },
+    S: {
+        name: "cardS",
+        model: "./assets/model/cards/cardS.glb",
+        position: new THREE.Vector3(-30, 4, 225)
+    },
+    T: {
+        name: "cardT",
+        model: "./assets/model/cards/cardT.glb",
+        position: new THREE.Vector3(-30, 4, 225)
+    },
+    U: {
+        name: "cardU",
+        model: "./assets/model/cards/cardU.glb",
+        position: new THREE.Vector3(-30, 4, 225)
+    },
+    V: {
+        name: "cardV",
+        model: "./assets/model/cards/cardV.glb",
+        position: new THREE.Vector3(-30, 4, 225)
+    },
+    W: {
+        name: "cardW",
+        model: "./assets/model/cards/cardW.glb",
+        position: new THREE.Vector3(-30, 4, 225)
+    },
+    X: {
+        name: "cardX",
+        model: "./assets/model/cards/cardX.glb",
+        position: new THREE.Vector3(-30, 4, 225)
+    },
+    Y: {
+        name: "cardY",
+        model: "./assets/model/cards/cardY.glb",
+        position: new THREE.Vector3(-30, 4, 225)
+    },
+    Z: {
+        name: "cardZ",
+        model: "./assets/model/cards/cardZ.gltf",
         position: new THREE.Vector3(-30, 4, 225)
     },
 }
