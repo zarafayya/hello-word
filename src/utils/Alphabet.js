d922c09813ff3be1724cc29622eb2eca16db2077
SRYian <92420947+SRYian@users.noreply.github.com>
2022-12-12 01:05:49 +0700

    feat: add more alphabets in utils

--- src/utils/Alphabet.js
+++ src/utils/Alphabet.js
@@ -1,46 +1,167 @@
 import * as THREE from "three";
 
 const Alphabet = {
-    // panduan koordinat, by Trelel129
-    // untuk setiap 10 alphabet pindah baris (koordinat z ditambah/dikurang)
-    // untuk setiap alphabet (misal dari a ke b) berjarak 35 koordinat x
-    // *per baris bisa berisi 10 alphabet atau 5 saja jika dirasa terlalu banyak
-    A: {
-        name: "A",
-        model: "./assets/model/alphabets/A.gltf",
-        color: "rgba(247, 96, 69, 1)",
-        position: new THREE.Vector3(360, 180, -600),
-    },
-    B: {
-        name: "B",
-        model: "./assets/model/alphabets/B.gltf",
-        color: "rgba(236, 136, 121, 1)",
-        position: new THREE.Vector3(460, 130, -480)
-    },
-    C: {
-        name: "C",
-        model: "./assets/model/alphabets/C.gltf",
-        color: "rgba(217, 18, 37, 1)",
-        position: new THREE.Vector3(380, 70, -320)
-    },
-    D: {
-        name: "D",
-        model: "./assets/model/alphabets/D.gltf",
-        color: "rgba(192, 64, 39, 1)",
-        position: new THREE.Vector3(-80, 4, 135)
-    },
-    E: {
-        name: "E",
-        model: "./assets/model/alphabets/E.gltf",
-        color: "rgba(236, 136, 121, 1)",
-        position: new THREE.Vector3(100, 4, 180)
-    },
-    F: {
-        name: "F",
-        model: "./assets/model/alphabets/F.gltf",
-        color: "rgba(217, 18, 37, 1)",
-        position: new THREE.Vector3(-30, 4, 225)
-    },
-}
+  // panduan koordinat, by Trelel129
+  // untuk setiap 10 alphabet pindah baris (koordinat z ditambah/dikurang)
+  // untuk setiap alphabet (misal dari a ke b) berjarak 35 koordinat x
+  // *per baris bisa berisi 10 alphabet atau 5 saja jika dirasa terlalu banyak
+  A: {
+    name: "A",
+    model: "./assets/model/alphabets/A.gltf",
+    color: "rgba(247, 96, 69, 1)",
+    position: new THREE.Vector3(360, 180, -600),
+  },
+  B: {
+    name: "B",
+    model: "./assets/model/alphabets/B.gltf",
+    color: "rgba(236, 136, 121, 1)",
+    position: new THREE.Vector3(460, 130, -480),
+  },
+  C: {
+    name: "C",
+    model: "./assets/model/alphabets/C.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(380, 70, -320),
+  },
+  D: {
+    name: "D",
+    model: "./assets/model/alphabets/D.gltf",
+    color: "rgba(192, 64, 39, 1)",
+    position: new THREE.Vector3(-80, 4, 135),
+  },
+  E: {
+    name: "E",
+    model: "./assets/model/alphabets/E.gltf",
+    color: "rgba(236, 136, 121, 1)",
+    position: new THREE.Vector3(100, 4, 180),
+  },
+  F: {
+    name: "F",
+    model: "./assets/model/alphabets/F.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(-30, 4, 225),
+  },
+  G: {
+    // Start from here
+    name: "G",
+    model: "./assets/model/alphabets/F.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(-30, 4, 225),
+  },
+  H: {
+    name: "H",
+    model: "./assets/model/alphabets/F.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(-30, 4, 225),
+  },
+  I: {
+    name: "I",
+    model: "./assets/model/alphabets/F.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(-30, 4, 225),
+  },
+  J: {
+    name: "J",
+    model: "./assets/model/alphabets/F.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(-30, 4, 225),
+  },
+  K: {
+    name: "K",
+    model: "./assets/model/alphabets/F.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(-30, 4, 225),
+  },
+  L: {
+    name: "L",
+    model: "./assets/model/alphabets/F.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(-30, 4, 225),
+  },
+  M: {
+    name: "M",
+    model: "./assets/model/alphabets/F.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(-30, 4, 225),
+  },
+  N: {
+    name: "N",
+    model: "./assets/model/alphabets/F.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(-30, 4, 225),
+  },
+  O: {
+    name: "O",
+    model: "./assets/model/alphabets/F.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(-30, 4, 225),
+  },
+  P: {
+    name: "P",
+    model: "./assets/model/alphabets/F.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(-30, 4, 225),
+  },
+  Q: {
+    name: "Q",
+    model: "./assets/model/alphabets/F.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(-30, 4, 225),
+  },
+  R: {
+    name: "R",
+    model: "./assets/model/alphabets/F.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(-30, 4, 225),
+  },
+  S: {
+    name: "S",
+    model: "./assets/model/alphabets/F.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(-30, 4, 225),
+  },
+  T: {
+    name: "T",
+    model: "./assets/model/alphabets/F.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(-30, 4, 225),
+  },
+  U: {
+    name: "U",
+    model: "./assets/model/alphabets/F.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(-30, 4, 225),
+  },
+  V: {
+    name: "V",
+    model: "./assets/model/alphabets/F.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(-30, 4, 225),
+  },
+  W: {
+    name: "W",
+    model: "./assets/model/alphabets/F.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(-30, 4, 225),
+  },
+  X: {
+    name: "X",
+    model: "./assets/model/alphabets/F.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(-30, 4, 225),
+  },
+  Y: {
+    name: "Y",
+    model: "./assets/model/alphabets/F.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(-30, 4, 225),
+  },
+  Z: {
+    name: "Z",
+    model: "./assets/model/alphabets/F.gltf",
+    color: "rgba(217, 18, 37, 1)",
+    position: new THREE.Vector3(-30, 4, 225),
+  },
+};
 
 export default Alphabet;
