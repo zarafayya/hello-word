d60a8435b56d1683534b273c3fe9eecf4618d27f
ryohilmi <ryodm.74@gmail.com>
2022-12-13 20:36:32 +0700

    feat: fade on click

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-import "./style.css";
-import * as THREE from "three";
-import Setup from "./utils/Setup";
-import Lighting from "./utils/Lighting";
-import { Model, ColorModel, RenderPlane } from "./utils/Model";
-import CameraSetup from "./utils/CameraSetup";
-import Alphabet from "./utils/Alphabet";
-import Card from "./utils/Card";
-import IntToChar from "./utils/IntToChar";
-import gsap from "gsap";
-import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
-import {AddOperation} from "three";
+import './style.scss';
+import * as THREE from 'three';
+import Setup from './utils/Setup';
+import Lighting from './utils/Lighting';
+import { Model, ColorModel, RenderPlane } from './utils/Model';
+import CameraSetup from './utils/CameraSetup';
+import Alphabet from './utils/Alphabet';
+import Card from './utils/Card';
+import IntToChar from './utils/IntToChar';
+import gsap from 'gsap';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
+import { AddOperation } from 'three';
+import './utils/Interfaces'
 
 // Modes
 // 1: Dev Mode
@@ -22,7 +23,7 @@ const tl = gsap.timeline();
 
 // Plane
 // Plane(scene);
-RenderPlane(scene, "./assets/model/terrain.glb", "terrain");
+RenderPlane(scene, './assets/model/terrain.glb', 'terrain');
 
 // Model & Position
 const gltfLoader = new GLTFLoader();
@@ -63,13 +64,13 @@ var initialCamRotation = [
   perspectiveCamera.rotation.x,
   perspectiveCamera.rotation.y,
   perspectiveCamera.rotation.z,
-]
+];
 
-var j = "A";
+var j = 'A';
 
 for (let index = 0; index < 26; index++) {
   drawAlphabet(j);
-  
+
   j = String.fromCharCode(j.charCodeAt(0) + 1);
 }
 
@@ -88,54 +89,53 @@ var right = false;
 var flag = 1;
 
 // Menulis tulisan mode kamera
-var text = document.createElement("div");
-text.style.position = "absolute";
+var text = document.createElement('div');
+text.style.position = 'absolute';
 text.style.width = 100;
 text.style.height = 100;
-text.style.backgroundColor = "blue";
-text.style.top = 0 + "px";
-text.style.left = 0 + "px";
+text.style.backgroundColor = 'blue';
+text.style.top = 0 + 'px';
+text.style.left = 0 + 'px';
 
 // Mengatur tombol wand
 let pofImg;
-document.getElementById("wand-button").onclick = () => {
-  pofImg = document.getElementById("pof-img");
-  pofImg.classList.toggle("elementToFadeInAndOut");
+document.getElementById('wand-button').onclick = () => {
+  pofImg = document.getElementById('pof-img');
+  pofImg.classList.toggle('elementToFadeInAndOut');
   if (cam == 3) {
     cam = 2;
-    if (scene.getObjectByName("A") === undefined) {
-      j = "A";
+    if (scene.getObjectByName('A') === undefined) {
+      j = 'A';
       for (let index = 0; index < 26; index++) {
-        console.log("[main.js] Wand Button - Add Alphabet: ", j);
+        console.log('[main.js] Wand Button - Add Alphabet: ', j);
         drawAlphabet(j);
         j = String.fromCharCode(j.charCodeAt(0) + 1);
       }
     }
-    if (scene.getObjectByName("cardA")) {
-      j = "A"
+    if (scene.getObjectByName('cardA')) {
+      j = 'A';
       for (let index = 0; index < 26; index++) {
-        let card = "card" + j;
-        console.log("[main.js] Wand Button - Remove Card: ", card);
+        let card = 'card' + j;
+        console.log('[main.js] Wand Button - Remove Card: ', card);
         scene.remove(scene.getObjectByName(card));
         j = String.fromCharCode(j.charCodeAt(0) + 1);
       }
     }
-  }
-  else {
+  } else {
     cam = 3;
-    if (scene.getObjectByName("cardA") === undefined) {
-      j = "A";
+    if (scene.getObjectByName('cardA') === undefined) {
+      j = 'A';
       for (let index = 0; index < 26; index++) {
-        console.log("[main.js] Wand Button - Add Card: ", j);
+        console.log('[main.js] Wand Button - Add Card: ', j);
         drawCard(j);
         j = String.fromCharCode(j.charCodeAt(0) + 1);
       }
     }
 
-    if (scene.getObjectByName("A")) {
-      j = "A";
+    if (scene.getObjectByName('A')) {
+      j = 'A';
       for (let index = 0; index < 26; index++) {
-        console.log("[main.js] Wand Button - Remove Alphabet: ", j);
+        console.log('[main.js] Wand Button - Remove Alphabet: ', j);
         scene.remove(scene.getObjectByName(j));
         j = String.fromCharCode(j.charCodeAt(0) + 1);
       }
@@ -158,11 +158,12 @@ function rotate(rotation) {
   var y1 = rotation.y + initialCamRotation[1];
   var z1 = rotation.z + initialCamRotation[2];
 
-  if(
+  if (
     x1.toFixed(4) == perspectiveCamera.rotation.x.toFixed(4) &&
     y1.toFixed(4) == perspectiveCamera.rotation.y.toFixed(4) &&
     z1.toFixed(4) == perspectiveCamera.rotation.z.toFixed(4)
-  ) return;
+  )
+    return;
 
   tl.to(perspectiveCamera.rotation, {
     duration: 2,
@@ -173,116 +174,112 @@ function rotate(rotation) {
 }
 
 // Set Mode Kamera
-window.addEventListener("keydown", (e) => {
+window.addEventListener('keydown', (e) => {
   switch (e.key) {
-    case "1":
+    case '1':
       cam = 1;
       break;
-    case "2":
+    case '2':
       cam = 2;
       break;
-    case "3":
+    case '3':
       cam = 3;
       break;
   }
 });
 
 // Membaca saat tombol ditekan
-window.addEventListener("keydown", (e) => {
+window.addEventListener('keydown', (e) => {
   if (cam === 1) {
     // Dev Mode
     switch (e.key) {
-      case "w":
+      case 'w':
         forward = true;
         break;
-      case "s":
+      case 's':
         back = true;
         break;
-      case "a":
+      case 'a':
         left = true;
         break;
-      case "d":
+      case 'd':
         right = true;
         break;
     }
   } else if (cam === 2) {
-    
-    if (scene.getObjectByName("A") === undefined) {
-      j = "A";
+    if (scene.getObjectByName('A') === undefined) {
+      j = 'A';
       for (let index = 0; index < 26; index++) {
-        console.log("[main.js] Key Down EL - Add Alphabet: ", j);
+        console.log('[main.js] Key Down EL - Add Alphabet: ', j);
         drawAlphabet(j);
         j = String.fromCharCode(j.charCodeAt(0) + 1);
       }
     }
-    if (scene.getObjectByName("cardA")) {
-      j = "A"
+    if (scene.getObjectByName('cardA')) {
+      j = 'A';
       for (let index = 0; index < 26; index++) {
-        let card = "card" + j;
-        console.log("[main.js] Key Down EL - Remove Card: ", card);
+        let card = 'card' + j;
+        console.log('[main.js] Key Down EL - Remove Card: ', card);
         scene.remove(scene.getObjectByName(card));
         j = String.fromCharCode(j.charCodeAt(0) + 1);
       }
     }
 
     switch (e.key) {
-      case "ArrowRight":
+      case 'ArrowRight':
         if (flag == 26) {
           flag = 1;
-        }
-        else flag++;
+        } else flag++;
         break;
-      case "ArrowLeft":
+      case 'ArrowLeft':
         if (flag == 1) {
           flag = 26;
-        }
-        else flag--;
+        } else flag--;
         break;
     }
 
     var currentAlphabet = IntToChar(flag - 1);
     rotate(CameraSetup[currentAlphabet].rotation);
     translate(CameraSetup[currentAlphabet].position);
-
   } else if (cam === 3) {
-    if (scene.getObjectByName("cardA") === undefined) {
-      j = "A";
+    if (scene.getObjectByName('cardA') === undefined) {
+      j = 'A';
       for (let index = 0; index < 26; index++) {
-        console.log("[main.js] Key Down EL - Add Card: ", j);
+        console.log('[main.js] Key Down EL - Add Card: ', j);
         drawCard(j);
         j = String.fromCharCode(j.charCodeAt(0) + 1);
       }
     }
 
-    if (scene.getObjectByName("A")) {
-      j = "A";
+    if (scene.getObjectByName('A')) {
+      j = 'A';
       for (let index = 0; index < 26; index++) {
-        console.log("[main.js] Key Down EL - Remove Alphabet: ", j);
+        console.log('[main.js] Key Down EL - Remove Alphabet: ', j);
         scene.remove(scene.getObjectByName(j));
         j = String.fromCharCode(j.charCodeAt(0) + 1);
       }
     }
 
-    scene.remove(scene.getObjectByName("A"));
-    scene.remove(scene.getObjectByName("B"));
+    scene.remove(scene.getObjectByName('A'));
+    scene.remove(scene.getObjectByName('B'));
   }
 });
 
 // Membaca saat tombol berhenti ditekan
-window.addEventListener("keyup", (e) => {
+window.addEventListener('keyup', (e) => {
   if (cam === 1) {
     // Dev Mode
     switch (e.key) {
-      case "w":
+      case 'w':
         forward = false;
         break;
-      case "s":
+      case 's':
         back = false;
         break;
-      case "a":
+      case 'a':
         left = false;
         break;
-      case "d":
+      case 'd':
         right = false;
         break;
     }
@@ -292,12 +289,12 @@ window.addEventListener("keyup", (e) => {
 // Map Texture
 const loader = new THREE.CubeTextureLoader();
 const skyBox = loader.load([
-  "./assets/img/px.png",
-  "./assets/img/nx.png",
-  "./assets/img/py.png",
-  "./assets/img/ny.png",
-  "./assets/img/pz.png",
-  "./assets/img/nz.png",
+  './assets/img/px.png',
+  './assets/img/nx.png',
+  './assets/img/py.png',
+  './assets/img/ny.png',
+  './assets/img/pz.png',
+  './assets/img/nz.png',
 ]);
 scene.background = skyBox;
 
@@ -307,13 +304,13 @@ function animate() {
   renderer.render(scene, perspectiveCamera);
   switch (cam) {
     case 1:
-      text.innerHTML = "Dev Camera";
+      text.innerHTML = 'Dev Camera';
       break;
     case 2:
-      text.innerHTML = "Alphabet Camera";
+      text.innerHTML = 'Alphabet Camera';
       break;
     case 3:
-      text.innerHTML = "Card Camera";
+      text.innerHTML = 'Card Camera';
       break;
   }
 
---
+++ src/utils/Interfaces.js
@@ -0,0 +1,6 @@
+const start = document.querySelector('#start');
+const startButton = document.querySelector('#startButton');
+
+startButton.addEventListener('click', () => {
+  start.classList.add('hidden');
+});
