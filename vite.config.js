533048bc44f735d3483db16531ac44bed4738cb4
zarafayya <zahra.fayyadiyati@gmail.com>
2025-06-14 23:57:18 +0700

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  server: {
+    open: true,
+  },
 });
